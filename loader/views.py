1c8c7bff071516bd5982b4f07b675d0bdffbc63a
andrey.isaev <an.isaev7@gmail.com>
2022-09-19 12:21:45 +0300

    fix: pep8 formating

--- loader/views.py
+++ loader/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, request
+from functions import loger
 
-from loader.utils import add_post_to_json
+from loader.utils import add_post_to_json, check_extension
 
-loader_blueprint = Blueprint("loader_blueprint", __name__, template_folder="../loader/templates")
+loader_blueprint = Blueprint("loader_blueprint", __name__, template_folder="templates")
 
 
 @loader_blueprint.route("/post")
@@ -12,14 +13,27 @@ def post_page():
 
 @loader_blueprint.route("/post", methods=["POST"])
 def add_post():
-    content = request.form["content"]
+    content: str = request.form["content"]
     picture = request.files.get("picture")
+
     filename = picture.filename
+
+    if not check_extension(filename):
+        loger.info(f"{filename} is not a picture")
+        return "Not available file format,<br>Please upload JPEG or PNG"
+
     upload_path = "./uploads/images/"
-    picture.save(upload_path + filename)
+
+    try:
+        picture.save(upload_path + filename)
+    except Exception:
+        loger.error(f"Error with uploading file {filename}")
+        return "Error with uploading file"
+
     post = {
         "pic": "/uploads/images/" + filename,
         "content": content
     }
     add_post_to_json(post)
+
     return render_template("post_uploaded.html", post=post)
