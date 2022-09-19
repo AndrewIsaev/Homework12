e7ef27da938c8faa18021ab876dd0203522110f7
andrey.isaev <an.isaev7@gmail.com>
2022-09-19 12:31:22 +0300

    fix: formater in loger

--- functions.py
+++ functions.py
@@ -1,1 +1,8 @@
-import json
+import logging
+
+loger = logging.getLogger("project_loger")
+loger.setLevel(logging.INFO)
+loger_handler = logging.FileHandler("log.log")
+loger_formatter = logging.Formatter("%(levelname)s : %(asctime)s : %(message)s", datefmt="%d/%m/%Y %H:%M:%S")
+loger_handler.setFormatter(loger_formatter)
+loger.addHandler(loger_handler)
