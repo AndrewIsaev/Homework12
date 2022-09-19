fd5e3fba8372bf787fb26796d2aa54b16f572021
andrey.isaev <an.isaev7@gmail.com>
2022-09-19 12:28:32 +0300

    fix: add docstrings and typing

--- main/utils.py
+++ main/utils.py
@@ -1,17 +1,24 @@
 import json
+from typing import List, Dict
 
 PATH = "posts.json"
 
 
-def load_json(path):
+def load_json(path: str) -> List[Dict]:
+    """
+    Load data from json
+    :param path: json path
+    :return: List with posts
+    """
     with open(path, encoding="utf-8") as file:
         return json.load(file)
 
 
-def search_by_word(word):
+def search_by_word(word: str) -> List[Dict]:
+    """
+    Search posts by search_key
+    :param word: User word
+    :return: List with find posts
+    """
     result = [post for post in load_json(PATH) if word.lower() in post["content"].lower()]
     return result
-
-
-# print(load_jon(PATH))
-# print(search_by_word("Ага"))
