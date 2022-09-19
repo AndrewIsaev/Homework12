3c721a0d864e3308b91b16e31a7edc352071b249
andrey.isaev <an.isaev7@gmail.com>
2022-09-19 12:29:28 +0300

    fix: pep8

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, request
-
+from json import JSONDecodeError
 from main.utils import search_by_word
+from functions import loger
 
-main_blueprint = Blueprint("main_blueprint", __name__, template_folder="./templates")
+main_blueprint = Blueprint("main_blueprint", __name__, template_folder="templates")
+
 
 @main_blueprint.route("/")
 def main_page():
@@ -12,5 +14,12 @@ def main_page():
 @main_blueprint.route("/search/")
 def page_search():
     search_key = request.args.get("s")
-    posts = search_by_word(search_key)
-    return render_template("post_list.html", posts=posts, key=search_key)
+    try:
+        posts = search_by_word(search_key)
+        loger.info(f"Search by {search_key}")
+    except FileNotFoundError:
+        return "File Not Found"
+    except JSONDecodeError:
+        return "Can`t be convert to JSON"
+    else:
+        return render_template("post_list.html", posts=posts, key=search_key)
